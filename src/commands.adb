c9c68267cce75f2fef4a72b4f0280f752b6005d1
thindil@laeran.pl <thindil@laeran.pl>
2019-09-26 05:49:47 +0000

    added support for reading arguments from command line
    FossilOrigin-Name: 0f438b2661ec7acbdfa616db089996284d8adfa0142e38aba4e7398b2f35df64

--- src/commands.adb
+++ src/commands.adb
@@ -13,6 +13,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+with Ada.Characters.Handling; use Ada.Characters.Handling;
 with Ada.Command_Line; use Ada.Command_Line;
 with Ada.Directories; use Ada.Directories;
 with Ada.Strings; use Ada.Strings;
@@ -26,13 +27,38 @@ package body Commands is
       Key: constant Unbounded_String := To_Unbounded_String(Argument(1));
       Success: Boolean;
       SubTokens: Slice_Set;
-      Command, Arguments: Unbounded_String := Null_Unbounded_String;
+      Command, Arguments, ArgumentNumber: Unbounded_String :=
+        Null_Unbounded_String;
       ArgumentsStarts: Slice_Number;
+      VariableStarts, NumberPosition: Natural := 1;
    begin
       for Execute of Commands_List(Key).Execute loop
          if Length(Execute) = 0 then
             goto End_Of_Loop;
          end if;
+         loop
+            VariableStarts := Index(Execute, "$", VariableStarts);
+            exit when VariableStarts = 0;
+            if Is_Digit(Element(Execute, VariableStarts + 1)) then
+               NumberPosition := VariableStarts + 1;
+               ArgumentNumber := Null_Unbounded_String;
+               loop
+                  Append(ArgumentNumber, Element(Execute, NumberPosition));
+                  NumberPosition := NumberPosition + 1;
+                  exit when not Is_Digit(Element(Execute, NumberPosition));
+               end loop;
+               if Argument_Count <=
+                 Positive'Value(To_String(ArgumentNumber)) then
+                  Put_Line
+                    ("You didn't entered enough arguments for this command. Please check it description for information what should be entered.");
+                  return;
+               end if;
+               Replace_Slice
+                 (Execute, VariableStarts, NumberPosition - 1,
+                  Argument(Positive'Value(To_String(ArgumentNumber)) + 1));
+               VariableStarts := VariableStarts + 1;
+            end if;
+         end loop;
          Create(SubTokens, To_String(Execute), " ");
          if Slice(SubTokens, 1)'Length > 0 then
             Append(Command, Locate_Exec_On_Path(Slice(SubTokens, 1)).all);
@@ -51,7 +77,7 @@ package body Commands is
                Put_Line
                  ("Directory: " & Current_Directory & Directory_Separator &
                   To_String(Trim(Arguments, Both)) & " doesn't exists.");
-               exit;
+               return;
             end if;
             Set_Directory
               (Current_Directory & Directory_Separator &
@@ -63,7 +89,7 @@ package body Commands is
             Argument_String_To_List(To_String(Arguments)).all, Success);
          if not Success then
             Put_Line("Error during executing '" & To_String(Command) & "'");
-            exit;
+            return;
          end if;
          <<End_Of_Loop>>
          Command := Null_Unbounded_String;
