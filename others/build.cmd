2fdc7d7c77028bc5c0710629c87c375f8468bc34
thindil@laeran.pl <thindil@laeran.pl>
2019-09-30 05:49:51 +0000

    added command to build Windows version on Linux
    FossilOrigin-Name: 35f1fdff6908cd78dbcf75976ce9bdefce513a1430c165c07f399016c66ce804

---
+++ others/build.cmd
@@ -0,0 +1,8 @@
+@ECHO OFF
+set PATH=%PATH%;C:/GNAT/2019/bin
+gprbuild -P bob.gpr -XMode=release
+XCOPY /S bin release\
+XCOPY README.md release\doc\
+XCOPY COPYING release\doc\
+XCOPY CHANGELOG.md release\doc\
+gprclean -P bob.gpr
