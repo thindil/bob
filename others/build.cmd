7c785847cea1dae8dc9e2f96df6a58fcd09d6b93
thindil@laeran.pl <thindil@laeran.pl>
2020-10-18 08:38:19 +0000

    removed unneeded scripts
    FossilOrigin-Name: 75ebaa476cdc01d72c9ee4412910e11c1058738de35771b0ad97d8f7a813f0e9

--- others/build.cmd
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-set PATH=%PATH%;C:/GNAT/2019/bin
-gprbuild -P bob.gpr -XMode=release
-XCOPY /S bin release\
-XCOPY README.md release\doc\
-XCOPY COPYING release\doc\
-XCOPY CHANGELOG.md release\doc\
-XCOPY CONTRIBUTING.md release\doc\
-gprclean -P bob.gpr
