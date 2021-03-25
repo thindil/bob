8136e8e75bbaaa56ed0a96c4d228746eb2b325c4
thindil <thindil@laeran.pl>
2021-03-25 06:34:18 +0000

    fixed location of documentation during build releases
    FossilOrigin-Name: 9816e5b286579230cacc782d3a1d763b9ec1c51c0e8dbf4dcd068cda2fd219fd

--- others/build.tcl
+++ others/build.tcl
@@ -30,12 +30,12 @@ exec gprclean -P bob.gpr --target=$target >@stdout
 exec gprbuild -p -P bob.gpr -XMode=release --target=$target >@stdout
 puts -nonewline {Copying files and directories ... }
 if {$target != "x86_64-windows"} {
-   file mkdir usr/bin usr/share/doc
+   file mkdir usr/bin usr/share/doc/bob
    file copy bin/bob usr/bin
-   file copy README.md usr/share/doc
-   file copy COPYING usr/share/doc
-   file copy CHANGELOG.md usr/share/doc
-   file copy CONTRIBUTING.md usr/share/doc
+   file copy README.md usr/share/doc/bob
+   file copy COPYING usr/share/doc/bob
+   file copy CHANGELOG.md usr/share/doc/bob
+   file copy CONTRIBUTING.md usr/share/doc/bob
 } else {
    file mkdir [file join release doc]
    file copy bin [file join release bin]
